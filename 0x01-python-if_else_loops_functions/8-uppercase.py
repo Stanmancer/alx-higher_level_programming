2680484c1c4cf836f89c712a57a7e982a617240a
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-09-09 06:17:46 +0000

    [feat]: print a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyython3
+
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            i = chr(ord(i) - (ord('a') - ord('A')))
+
+        print("{}".format(i), end='')
+
+    print()
